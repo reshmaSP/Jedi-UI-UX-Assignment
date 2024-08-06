83e31d80ed89dd42b5cd66d245aae9ecf12a06d0
reshmaSP <panigrahireshma1@gmail.com>
2024-08-06 15:52:10 +0530

    Completed a basic UI, using git life cycle commands

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Jedi-UI-UX-Assignment
+Hello!!!
+This is Reshma Panigrahi (SDE-1) at Flipkart
+![alt text](image.png)
---
+++ demo.js
@@ -0,0 +1,6 @@
+// Optional: Add any JavaScript you want here
+// For now, we'll just log a message to the console
+
+console.log("Jedi UI-UX training")
+alert(" welcome to Reshma's nature page ")
+console.log("Page loaded successfully");
